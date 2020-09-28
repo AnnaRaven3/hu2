076f356ece6eb48f530fc14de9469d525aa5ffed
AnnaRaven3 <Schmidinger.Anna@sus.htl-grieskirchen.at>
2020-09-28 14:22:43 +0200

    NumberTester methoden erstellen

---
+++ src/net/htl/grieskirchen/pos/aschmidinger18/hausuebung2/NumberTester.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htl.grieskirchen.pos.aschmidinger18.hausuebung2;
+
+/**
+ *
+ * @author annas
+ */
+public class NumberTester {
+    
+    public NumberTester(String fileName){}
+    
+    public void setOddEvenTester(NumberTest oddTester){
+        
+    }
+    
+    public void setPrimeTester(NumberTest primeTester){
+        
+    }
+    
+    public void setPalindromeTester(NumberTest palidromeTester){
+        
+    }
+    
+    public void testFile(){
+        
+    }
+    
+    public interface NumberTest{
+        boolean testNumber(int number);
+    }
+}
