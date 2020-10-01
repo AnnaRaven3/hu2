1a22a872d6e575b7aa67e4064129aaad91c3b62a
AnnaRaven3 <Schmidinger.Anna@sus.htl-grieskirchen.at>
2020-10-01 16:48:19 +0200

    Lambda palindromeTester

--- src/net/htl/grieskirchen/pos/aschmidinger18/hausuebung2/Main.java
+++ src/net/htl/grieskirchen/pos/aschmidinger18/hausuebung2/Main.java
@@ -31,5 +31,23 @@ public class Main {
             }
             return true;
         });
+        
+        nt.setPalindromeTester((p) -> {
+            String text = Integer.toString(p);
+            String clean = text.replaceAll("\\s+", "").toLowerCase();
+            int length = clean.length();
+            int forward = 0;
+            int backward = length - 1;
+            while (backward > forward) {
+                char forwardChar = clean.charAt(forward++);
+                char backwardChar = clean.charAt(backward--);
+                if (forwardChar != backwardChar) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        nt.testFile();
     }
 }
