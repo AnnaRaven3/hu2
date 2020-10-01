075b089a31964c850c26a4f87be12a5e6a317ab4
AnnaRaven3 <Schmidinger.Anna@sus.htl-grieskirchen.at>
2020-10-01 17:03:53 +0200

    Klasse Number und CalculateOperation

---
+++ src/net/htl/grieskirchen/pos/aschmidinger18/hausuebung2/CalculateOperation.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htl.grieskirchen.pos.aschmidinger18.hausuebung2;
+
+/**
+ *
+ * @author annas
+ */
+public interface CalculateOperation {
+    
+    Number calc (Number x , Number y ) ;
+    
+}
